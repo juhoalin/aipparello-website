055c2e31b3305743a88630d84e4ccad3062784c5
juhoalin <63160665+juhoalin@users.noreply.github.com>
2024-01-22 11:25:41 +0200

    initial commit

---
+++ src/configuration.js
@@ -0,0 +1,11 @@
+require("./configuration.css");
+require("./api.js");
+
+// Get the element with id "quiz-content"
+const quizContent = document.getElementById("quiz-content");
+
+// Require the quiz.html file
+const quizHTML = require("./configuration.html");
+
+// Inject the HTML into the quiz-content element
+quizContent.innerHTML = quizHTML.default;
---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    plugins: [
+        new MiniCssExtractPlugin(),
+        new HtmlWebpackPlugin({
+            template: "./src/configuration.html",
+            filename: "webflow.serve.html",
+            inject: true,
+        }),
+    ],
+    mode: "production",
+    entry: {
+        ["webflow.serve"]: "./src/configuration.js",
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: [MiniCssExtractPlugin.loader, "css-loader"],
+            },
+            {
+                test: /\.html$/i,
+                loader: "html-loader",
+            },
+        ],
+    },
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "[name].js",
+        library: "[name]",
+        libraryTarget: "umd",
+        globalObject: "this",
+        umdNamedDefine: true,
+        clean: true,
+    },
+};
